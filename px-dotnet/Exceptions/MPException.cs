d6b9e57cc3187ff1e441294a4fcded021b7ce3a4
hdlopez <hdlopez@gmail.com>
2016-11-17 23:27:32 -0300

    Initial commit

---
+++ px-dotnet/Exceptions/MPException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace px_dotnet.Exceptions
+{
+	class MPException : Exception
+	{
+		private string RequestId { get; private set; }
+		private int? StatusCode { get; private set; }
+
+		public MPException(string message) : base(message) {
+		}
+
+		public MPException(string message, Exception innerException) : base(message, innerException) {
+		}
+
+		public MPException(string message, string requestId, int? statusCode) : base(message) {
+			RequestId = requestId;
+			StatusCode = statusCode;
+		}
+
+		public MPException(String message, String requestId, int statusCode, Exception innerException)
+					: base(message, innerException){
+			RequestId = requestId;
+			StatusCode = statusCode;
+		}
+
+		public override string ToString() {
+			string reqIdStr = "";
+			if (string.IsNullOrEmpty(RequestId))
+				reqIdStr = "; request-id: " + RequestId;
+			string statCodeStr = "";
+			if (StatusCode.HasValue)
+				statCodeStr = "; status_code: " + StatusCode.Value;
+			return base.ToString() + reqIdStr + statCodeStr;
+		}
+	}
+}
---
+++ px-dotnet/MPConf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace px_dotnet
+{
+	public class MPConf
+	{
+		private const String DEFAULT_BASE_URL = "https://api.mercadopago.com";
+
+		private static String clientSecret = null;
+		private static String clientId = null;
+		private static String accessToken = null;
+		private static String appId = null;
+		private static String baseUrl = DEFAULT_BASE_URL;
+
+		/// <summary>  
+		///  Getter/Setter for ClientSecret 
+		/// </summary>  
+		public static string GetClientSecret()
+		{
+			return clientSecret;
+		}
+	}
+
+}
