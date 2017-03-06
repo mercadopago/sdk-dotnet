5a490865f3119c058e2df53a72f871d82f3b1cd7
Joel Ibaceta <mail@joelibaceta.com>
2017-03-06 12:25:56 -0300

    fix: Mercadopago => MercadoPago

--- MercadoPagoSDK.Test/Exceptions/MPExceptionTest.cs
+++ MercadoPagoSDK.Test/Exceptions/MPExceptionTest.cs
@@ -11,16 +11,16 @@ namespace MercadoPagoSDK.Test
 		public void MPExceptionTestToStringOverride()
 		{
 			MPException exception = new MPException("Some Exception message");
-			Assert.AreEqual("Mercadopago.MPException: Some Exception message", exception.ToString().Trim());
+			Assert.AreEqual("MercadoPago.MPException: Some Exception message", exception.ToString().Trim());
 
 			MPException exception2 = new MPException("Some Exception message 2", new MPException("InnerExceptionMessage 2"));
-			Assert.AreEqual("Mercadopago.MPException: InnerExceptionMessage 2", exception2.InnerException.ToString().Trim());
+			Assert.AreEqual("MercadoPago.MPException: InnerExceptionMessage 2", exception2.InnerException.ToString().Trim());
 
 			MPException exception3 = new MPException("Some Exception message 3", "requestId", 666);
-			Assert.AreEqual(string.Format("Mercadopago.MPException: Some Exception message 3; request-id: {0}; status_code: {1}", "requestId", 666), exception3.ToString().Trim());
+			Assert.AreEqual(string.Format("MercadoPago.MPException: Some Exception message 3; request-id: {0}; status_code: {1}", "requestId", 666), exception3.ToString().Trim());
 
 			MPException exception4 = new MPException("Some Exception message 4", "requestId", 666, new MPException("InnerExceptionMessage 4"));
-			Assert.AreEqual("Mercadopago.MPException: InnerExceptionMessage 4", exception4.InnerException.ToString().Trim());
+			Assert.AreEqual("MercadoPago.MPException: InnerExceptionMessage 4", exception4.InnerException.ToString().Trim());
 		}
 	}
 }
