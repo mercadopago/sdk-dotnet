64a84ddd82335a0437d7487a994d62b34e6c5683
Joel Ibaceta <joel.ibaceta@mercadolibre.com>
2017-10-25 11:52:44 -0300

    minimal style fixes for payment resource

--- px-dotnet/Resources/Payment.cs
+++ px-dotnet/Resources/Payment.cs
@@ -11,28 +11,29 @@ using Newtonsoft.Json.Converters;
 
 namespace MercadoPago.Resources
 {
+
+    /// <summary>
+    /// This service allows you to create, modify or read payments
+    /// </summary>
     public class Payment : MPBase
     {
-        #region Actions
-
+        #region Actions 
         /// <summary>
         /// Find a payment trought an unique identifier
         /// </summary>
         [GETEndpoint("/v1/payments/:id")]
-        public static Payment FindById(string id)
+        public static Payment FindById(int? id)
         {
-            return (Payment)ProcessMethod<Payment>(typeof(Payment), "FindById", id, false);
-        }
-
+            return (Payment)ProcessMethod<Payment>(typeof(Payment), "FindById", id.ToString(), false);
+        } 
         /// <summary>
-        /// Find a payment trought an unique identifier
+        /// Find a payment trought an unique identifier with Local Cache Flag
         /// </summary>
         [GETEndpoint("/v1/payments/:id")]
-        public static Payment FindById(string id, bool useCache)
+        public static Payment FindById(int? id, bool useCache)
         {
-            return (Payment)ProcessMethod<Payment>(typeof(Payment), "FindById", id, useCache);
-        }
-
+            return (Payment)ProcessMethod<Payment>(typeof(Payment), "FindById", id.ToString(), useCache);
+        } 
         /// <summary>
         /// Save a new payment
         /// </summary>
@@ -40,8 +41,7 @@ namespace MercadoPago.Resources
         public Payment Save()
         {
             return (Payment)ProcessMethod<Payment>("Save", WITHOUT_CACHE);  
-        }
-
+        } 
         /// <summary>
         /// Update editable properties
         /// </summary>
@@ -49,24 +49,21 @@ namespace MercadoPago.Resources
         public Payment Update()
         {
             return (Payment)ProcessMethod<Payment>("Update", WITHOUT_CACHE);
-        }
-
+        } 
         /// <summary>
         /// Get all payments
         /// </summary>
         public static List<Payment> All()
         {
             return All(WITHOUT_CACHE);
-        }
-
+        } 
         /// <summary>
         /// Get all payments acoording to specific filters
         /// </summary>
         public static List<Payment> Search(Dictionary<string, string> filters)
         {
             return Search(filters, WITHOUT_CACHE);
-        }
-
+        } 
         /// <summary>
         /// Get all payments, with using cache option
         /// </summary>
@@ -74,8 +71,7 @@ namespace MercadoPago.Resources
         public static List<Payment> All(bool useCache)
         {
             return (List<Payment>)ProcessMethodBulk<Payment>(typeof(Payment), "Search", useCache);
-        }
-       
+        } 
         /// <summary>
         /// Get all payments acoording to specific filters, with using cache option
         /// </summary>
@@ -83,18 +79,17 @@ namespace MercadoPago.Resources
         public static List<Payment> Search(Dictionary<string, string> filters, bool useCache)
         {
             return (List<Payment>)ProcessMethodBulk<Payment>(typeof(Payment), "Search", filters, useCache);
-        } 
-
+        }  
         #endregion
 
-        #region Properties
-
+        #region Properties 
         private int? _id;
         private DateTime? _date_created;
         private DateTime? _date_approved;
         private DateTime? _date_last_updated;
         private DateTime? _money_release_date;
         private int? _collector_id;
+        [JsonConverter(typeof(StringEnumConverter))]
         private OperationType? _operation_type;        
         private Payer _payer;
         private bool? _binary_mode;
@@ -104,6 +99,7 @@ namespace MercadoPago.Resources
         private string _description;
         private JObject _metadata;              
         [StringLength(3)]
+        [JsonConverter(typeof(StringEnumConverter))]
         private CurrencyId? _currency_id;
         private float? _transaction_amount;
         private float? _transaction_amount_refunded;
@@ -113,32 +109,28 @@ namespace MercadoPago.Resources
         private TransactionDetail _transaction_details;
         private FeeDetail[] _fee_details;
         private int? _differential_pricing_id;
-        private float? _application_fee;
-
+        private float? _application_fee; 
         [JsonConverter(typeof(StringEnumConverter))]
-        private PaymentStatus? _status;        
-
-        private string _status_detail ;
+        private PaymentStatus? _status;         
+        private string _status_detail;
         private bool? _capture ;
-        private bool? _captured ;
-        private string _call_for_authorize_id ;
-        private string _payment_method_id ;
-        private string _issuer_id ;       
-        private PaymentTypeId? _payment_type_id ;        
+        private bool? _captured;
+        private string _call_for_authorize_id;
+        private string _payment_method_id;
+        private string _issuer_id;
+        [JsonConverter(typeof(StringEnumConverter))]
+        private PaymentTypeId? _payment_type_id;        
         private string _token ;
-        private DataStructures.Payment.Card? _card ;
-        private string _statement_descriptor ;
+        private DataStructures.Payment.Card? _card;
+        private string _statement_descriptor;
         private int? _installments ;
         private string _notification_url;
         private string _callback_url;
         private Refund[] _refunds ;
-        private AdditionalInfo? _additional_info ;
-
- 
+        private AdditionalInfo? _additional_info ; 
         #endregion
 
-        #region Accessors
-
+        #region Accessors 
         /// <summary>
         /// Payment identifier
         /// </summary>
@@ -146,8 +138,7 @@ namespace MercadoPago.Resources
         {
             get { return this._id; }
             private set { this._id = value; }
-        }
-
+        } 
         /// <summary>
         /// Payment’s creation date
         /// </summary>
@@ -155,8 +146,7 @@ namespace MercadoPago.Resources
         {
             get { return this._date_created; }
             private set { this._date_created = value; }
-        }
-
+        } 
         /// <summary>
         /// Payment’s approval date
         /// </summary>
@@ -164,8 +154,7 @@ namespace MercadoPago.Resources
         {
             get { return this._date_approved; }
             private set { this._date_approved = value; }
-        }
-
+        } 
         /// <summary>
         /// Last modified date
         /// </summary>
@@ -173,8 +162,7 @@ namespace MercadoPago.Resources
         {
             get { return this._date_last_updated; }
             private set { this._date_last_updated = value; }
-        }
-
+        } 
         /// <summary>
         /// Release date of payment
         /// </summary>
@@ -182,8 +170,7 @@ namespace MercadoPago.Resources
         {
             get { return this._money_release_date; }
             private set { this._money_release_date = value; }
-        }
-
+        } 
         /// <summary>
         /// Identifies the seller
         /// </summary>
@@ -191,8 +178,7 @@ namespace MercadoPago.Resources
         {
             get { return this._collector_id; }
             private set { this._collector_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Payment type
         /// </summary>
@@ -200,18 +186,15 @@ namespace MercadoPago.Resources
         {
             get { return this._operation_type; }
             private set { this._operation_type = value; }
-        }
-
+        } 
         /// <summary>
-        /// Identifies the buyer 
-        ///  
+        /// Identifies the buyer  
         /// </summary>
         public Payer Payer 
         {
             get { return this._payer; }
             set { this._payer = value; }
-        }
-
+        } 
         /// <summary>
         /// When set to true, the payment can only be approved or rejected. Otherwise in_process status is added
         /// </summary>
@@ -219,8 +202,7 @@ namespace MercadoPago.Resources
         {
             get { return this._binary_mode; }
             set { this._binary_mode = value; }
-        }
-
+        } 
         /// <summary>
         /// Whether the payment will be processed in sandbox or in production mode
         /// </summary>
@@ -228,8 +210,7 @@ namespace MercadoPago.Resources
         {
             get { return this._live_mode; }
             private set { this._live_mode = value; }
-        }
-
+        } 
         /// <summary>
         /// Order identifier
         /// </summary>
@@ -237,8 +218,7 @@ namespace MercadoPago.Resources
         {
             get { return this._order; }
             set { this._order = value; }
-        }
-
+        } 
         /// <summary>
         /// ID given by the merchant in their system
         /// </summary>
@@ -246,8 +226,7 @@ namespace MercadoPago.Resources
         {
             get { return this._external_reference; }
             set { this._external_reference = value; }
-        }
-
+        } 
         /// <summary>
         /// Payment reason or item title
         /// </summary>
@@ -255,8 +234,7 @@ namespace MercadoPago.Resources
         {
             get { return this._description; }
             set { this._description = value; }
-        }
-
+        } 
         /// <summary>
         /// Valid JSON that can be attached to the payment to record additional attributes of the merchant
         /// </summary>
@@ -264,8 +242,7 @@ namespace MercadoPago.Resources
         {
             get { return this._metadata; }
             set { this._metadata = value; }
-        }
-
+        } 
         /// <summary>
         /// ID of the currency used in the payment
         /// </summary>
@@ -273,8 +250,7 @@ namespace MercadoPago.Resources
         {
             get { return this._currency_id; }
             private set { this._currency_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Product cost
         /// </summary>
@@ -282,8 +258,7 @@ namespace MercadoPago.Resources
         {
             get { return this._transaction_amount; }
             set { this._transaction_amount = value; }
-        }
-
+        } 
         /// <summary>
         /// Total refunded amount in this payment
         /// </summary>
@@ -291,8 +266,7 @@ namespace MercadoPago.Resources
         {
             get { return this._transaction_amount_refunded; }
             private set { this._transaction_amount_refunded = value; }
-        }
-
+        } 
         /// <summary>
         /// Amount of the coupon discount
         /// </summary>
@@ -300,8 +274,7 @@ namespace MercadoPago.Resources
         {
             get { return this._coupon_amount; }
             set { this._coupon_amount = value; }
-        }
-
+        } 
         /// <summary>
         /// Discount campaign ID
         /// </summary>
@@ -309,8 +282,7 @@ namespace MercadoPago.Resources
         {
             private get { return this._campaign_id; }
             set { this._campaign_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Discount campaign with a specific code
         /// </summary>
@@ -318,8 +290,7 @@ namespace MercadoPago.Resources
         {
             private get { return this._coupon_code; }
             set { this._coupon_code = value; }
-        }
-
+        } 
         /// <summary>
         /// Groups the details of the transaction
         /// </summary>
@@ -327,8 +298,7 @@ namespace MercadoPago.Resources
         {
             get { return this._transaction_details; }
             private set { this._transaction_details = value; }
-        }
-
+        } 
         /// <summary>
         /// List of fees
         /// </summary>
@@ -336,8 +306,7 @@ namespace MercadoPago.Resources
         {
             get { return this._fee_details; }
             private set { this._fee_details = value; }
-        }
-
+        } 
         /// <summary>
         /// Id of the scheme for the absorption of financing fee
         /// </summary>
@@ -345,8 +314,7 @@ namespace MercadoPago.Resources
         {
             get { return this._differential_pricing_id; }
             set { this._differential_pricing_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Fee collected by a marketplace or MercadoPago Application
         /// </summary>
@@ -354,8 +322,7 @@ namespace MercadoPago.Resources
         {
             private get { return this._application_fee; }
             set { this._application_fee = value; }
-        }
-
+        } 
         /// <summary>
         /// Payment status
         /// </summary>
@@ -363,8 +330,7 @@ namespace MercadoPago.Resources
         {
             get { return this._status; }
             set { this._status = value; }
-        }
-
+        } 
         /// <summary>
         /// Gives more detailed information on the current state or rejection cause
         /// </summary>
@@ -372,8 +338,7 @@ namespace MercadoPago.Resources
         {
             get { return this._status_detail; }
             private set { this._status_detail = value; }
-        }
-
+        } 
         /// <summary>
         /// Gives more detailed information on the current state or rejection cause
         /// </summary>
@@ -381,8 +346,7 @@ namespace MercadoPago.Resources
         {
             private get { return this._capture; }
             set { this._capture = value; }
-        }
-
+        } 
         /// <summary>
         /// Gives more detailed information on the current state or rejection cause
         /// </summary>
@@ -390,8 +354,7 @@ namespace MercadoPago.Resources
         {
             get { return this._captured; }
             private set { this._captured = value; }
-        }
-
+        } 
         /// <summary>
         /// Gives more detailed information on the current state or rejection cause
         /// </summary>
@@ -399,8 +362,7 @@ namespace MercadoPago.Resources
         {
             get { return this._call_for_authorize_id; }
             private set { this._call_for_authorize_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Gives more detailed information on the current state or rejection cause
         /// </summary>
@@ -408,8 +370,7 @@ namespace MercadoPago.Resources
         {
             get { return this._payment_method_id; }
             set { this._payment_method_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Payment method issuer
         /// </summary>
@@ -417,8 +378,7 @@ namespace MercadoPago.Resources
         {
             get { return this._issuer_id; }
             set { this._issuer_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Type of payment method chosen
         /// </summary>
@@ -426,8 +386,7 @@ namespace MercadoPago.Resources
         {
             get { return this._payment_type_id; }
             private set { this._payment_type_id = value; }
-        }
-
+        } 
         /// <summary>
         /// Card token ID
         /// </summary>
@@ -435,8 +394,7 @@ namespace MercadoPago.Resources
         {
             private get { return this._token; }
             set { this._token = value; }
-        }
-
+        } 
         /// <summary>
         /// Details of the card used
         /// </summary>
@@ -444,8 +402,7 @@ namespace MercadoPago.Resources
         {
             get { return this._card; }
             private set { this._card = value; }
-        }
-
+        } 
         /// <summary>
         /// How will look the payment in the card bill (e.g.: MERCADOPAGO)
         /// </summary>
@@ -453,8 +410,7 @@ namespace MercadoPago.Resources
         {
             get { return this._statement_descriptor; }
             set { this._statement_descriptor = value; }
-        }
-
+        } 
         /// <summary>
         /// Selected quantity of installments
         /// </summary>
@@ -462,8 +418,7 @@ namespace MercadoPago.Resources
         {
             get { return this._installments; }
             set { this._installments = value; }
-        }
-
+        } 
         /// <summary>
         /// URL where mercadopago will send notifications associated to changes in this payment
         /// </summary>
@@ -471,8 +426,7 @@ namespace MercadoPago.Resources
         {
             get { return this._notification_url; }
             set { this._notification_url = value; }
-        }
-
+        } 
         /// <summary>
         /// URL where mercadopago does the final redirect (only for bank transfers)
         /// </summary>
@@ -480,8 +434,7 @@ namespace MercadoPago.Resources
         {
             get { return this._callback_url; }
             set { this._callback_url = value; }
-        }
-
+        } 
         /// <summary>
         /// List of refunds that were made to this payment
         /// </summary>
@@ -489,8 +442,7 @@ namespace MercadoPago.Resources
         {
             get { return this._refunds; }
             private set { this._refunds = value; }
-        }
-
+        } 
         /// <summary>
         /// Data that could improve fraud analysis and conversion rates. Try to send as much information as possible.
         /// </summary>
@@ -498,8 +450,7 @@ namespace MercadoPago.Resources
         {
             private get { return this._additional_info; }
             set { this._additional_info = value; }
-        }
-        
+        } 
         #endregion
     }
 }
