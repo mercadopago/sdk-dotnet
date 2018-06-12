e1a45f9879270573af8f9e508d44878330e613d3
Janeé Peña <diana.enriquez@mercadolibre.com.mx>
2018-06-12 15:00:50 -0500

    Add example of API Checkout

--- MercadoPagoExample/Payments/APIPayment.cs
+++ MercadoPagoExample/Payments/APIPayment.cs
@@ -5,7 +5,26 @@ namespace MercadoPagoExample.Payments
     {
         public DoPayment()
         {
-            
+            MercadoPago.SDK.ClientSecret = "ACCESS_TOKEN_VALUE";
+
+            Payment payment = new Payment
+            {
+                TransactionAmount = (float)1000,
+                Token = "card_token_id",
+                Description = "Cats",
+                ExternalReference = "YOUR_REFERENCE",
+                PaymentMethodId = "visa",
+                Installments = 1,
+                Payer = new Payer {
+                    Email = "some@mail.com",
+                    FistName = "Diana",
+                    LastName = "Prince"
+                }
+            };
+
+            payment.Save();
+
+            return payment.Status;
         }
     }
 }
