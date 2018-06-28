509e6ff8d75a2f2987d7bd494b6ba2498f0638ed
Federico Berasategui <fberasategui@lexim.com.ar>
2018-06-28 11:01:14 -0300

    MercadoPagoExample - Web Checkout (#47)
    * Web Checkout example
    
    * cleanup
    
    * Cargar o pedir al usuario AccessToken/LiveToken dependiendo del ejemplo

--- MercadoPagoExample/Program.cs
+++ MercadoPagoExample/Program.cs
@@ -1,26 +1,51 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
+using MercadoPago;
+using MercadoPagoExample.Payments;
 
 namespace MercadoPagoExample
 {
-    class MainClass
+    public class MainClass
     {
-        public static void Main(string[] args)
-        { 
-
-            // TODO: Request or guess credentials from ENV variables 
-
+        private static decimal ShowMenu()
+        {
+            Console.Clear();
             Console.WriteLine("MercadoPago Examples");
             Console.WriteLine();
             Console.WriteLine("1. Pagos");
             Console.WriteLine("\t1.1. Pago con Checkout Web");
             Console.WriteLine("\t1.2. Pago via API");
-            Console.WriteLine("\t1.3. Pago con Tarjetas Guardadas"); 
+            Console.WriteLine("\t1.3. Pago con Tarjetas Guardadas");
             Console.WriteLine("5. Exit");
-
 
             var selection = Console.ReadLine();
 
+            return decimal.TryParse(selection, out var result)
+                ? result
+                : 0;
+        }
 
+        public static void Main(string[] args)
+        { 
+            while (true)
+            {
+                switch (ShowMenu())
+                {
+                    case 1.1m:
+                        WebCheckoutExample.Run();
+                        break;
+                    case 1.2m:
+                        ApiPaymentExample.Run();
+                        break;
+                    case 1.3m:
+                        SavedCardsExample.Run();
+                        break;
+                    case 5m:
+                        return;
+                }
+                Utils.Prompt("Presione Enter para volver al Menu: ");
+            }
         }
     }
 }
---
+++ MercadoPagoExample/Utils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using MercadoPago;
+
+namespace MercadoPagoExample
+{
+    public static class Utils
+    {
+        public static string Prompt(string text)
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine();
+        }
+
+        public static void LoadOrPromptAccessToken()
+        {
+            SDK.AccessToken  = LoadOrPrompt(SDK.AccessToken,  nameof(SDK.AccessToken),  "Ingrese Access Token: ");
+        }
+
+        public static void LoadOrPromptClientCredentials()
+        {
+            SDK.ClientId     = LoadOrPrompt(SDK.ClientId,     nameof(SDK.ClientId),     "Ingrese Client Id: ");
+            SDK.ClientSecret = LoadOrPrompt(SDK.ClientSecret, nameof(SDK.ClientSecret), "Ingrese Client Secret: ");
+            SDK.AppId        = LoadOrPrompt(SDK.AppId,        nameof(SDK.AppId),        "Ingrese App Id: ");
+        }
+
+        private static string LoadOrPrompt(string currentValue, string name, string prompt)
+        {
+            while (true)
+            {
+                var value = currentValue;
+
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+
+                value = ConfigurationManager.AppSettings[name];
+
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+
+                value = Environment.GetEnvironmentVariable(name);
+
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+
+                value = Prompt(prompt);
+
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+            }
+        }
+    }
+}
