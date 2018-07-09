6c85cfce63b23321f2afb621af4349401d0b6bc2
Federico Berasategui <fberasategui@lexim.com.ar>
2018-07-09 13:13:21 -0300

    IdentificationType Resource

---
+++ px-dotnet/Resources/New/IdentificationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MercadoPago.Resources.New
+{
+    public class IdentificationType : Resource<IdentificationType>
+    {
+        public static List<IdentificationType> GetAll(bool useCache = false) => GetList("/v1/identification_types", useCache);
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int MinLength { get; set; }
+
+        public int MaxLength { get; set; }
+    }
+}
