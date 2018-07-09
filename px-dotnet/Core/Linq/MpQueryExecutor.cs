67ddb883aba39873e80c7eed2080f06c046f7629
Federico Berasategui <fberasategui@lexim.com.ar>
2018-07-09 16:01:14 -0300

    LINQ Provider - Initial Commit - Needs Visitor

---
+++ px-dotnet/Core/Linq/MpQueryExecutor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Remotion.Linq;
+using Remotion.Linq.Clauses;
+
+namespace MercadoPago.Core.Linq
+{
+    internal class MpQueryExecutor<TResource> : IQueryExecutor where TResource: ResourceBase, new()
+    {
+        private readonly string _apiPath;
+        private readonly bool _useCache;
+
+        public MpQueryExecutor(string apiPath, bool useCache = false)
+        {
+            _apiPath = apiPath;
+            _useCache = useCache;
+        }
+
+        public IEnumerable<T> ExecuteCollection<T>(QueryModel queryModel)
+        {
+            if (typeof(T) != typeof(TResource))
+                throw new InvalidOperationException();
+            
+            return Query(queryModel).Cast<T>();
+        }
+
+        private IEnumerable<TResource> Query(QueryModel queryModel)
+        {
+            var queryParameters = new Dictionary<string, string>();
+
+            void ParseExpression(BinaryExpression binary)
+            {
+                if (binary.NodeType != ExpressionType.Equal)
+                    throw new InvalidOperationException();
+
+                if (!(binary.Left is MemberExpression left))
+                    throw new InvalidOperationException();
+
+                if (!(binary.Right is ConstantExpression right))
+                    throw new InvalidOperationException();
+
+                var key = left.Member.Name.ToSnakeCase();
+                var value = right.Value?.ToString();
+
+                if (!string.IsNullOrEmpty(value))
+                    queryParameters.Add(key, value);
+            }
+
+            var wheres = 
+                queryModel.BodyClauses
+                          .OfType<WhereClause>()
+                          .ToList();
+
+            foreach (var where in wheres)
+            {
+                if (!(where.Predicate is BinaryExpression binary))
+                    throw new InvalidOperationException();
+
+                ParseExpression(binary);
+            }
+
+            return Resource<TResource>.GetList(_apiPath, _useCache, queryParameters);
+        }
+
+        public T ExecuteSingle<T>(QueryModel queryModel, bool returnDefaultWhenEmpty)
+        {
+            var sequence = ExecuteCollection<T>(queryModel);
+
+            return returnDefaultWhenEmpty ? sequence.SingleOrDefault() : sequence.Single();
+        }
+
+        public T ExecuteScalar<T>(QueryModel queryModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ px-dotnet/Core/Linq/MpQueryable.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using Remotion.Linq;
+using Remotion.Linq.Parsing.Structure;
+
+namespace MercadoPago.Core.Linq
+{
+    internal class MpQueryable<T> : QueryableBase<T> where T: ResourceBase, new()
+    {
+        
+        public MpQueryable(string apiPath, bool useCache = false): base(QueryParser.CreateDefault(), new MpQueryExecutor<T>(apiPath, useCache))
+        {
+        }
+
+        // No quitar constructores. Los necesita ReLinq.
+        public MpQueryable(IQueryParser queryParser, IQueryExecutor executor)
+            : base(new DefaultQueryProvider(typeof(MpQueryable<>), queryParser, executor))
+        {
+        }
+
+        public MpQueryable(IQueryProvider provider, Expression expression)
+            : base(provider, expression)
+        {
+        }
+    }
+}
