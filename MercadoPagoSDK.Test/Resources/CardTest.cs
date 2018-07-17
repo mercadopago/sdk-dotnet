87f8039bbc596940802ba1091c216f3d0e166c8a
Federico Berasategui <fberasategui@lexim.com.ar>
2018-07-17 03:01:25 -0300

    Reenabled Card tests

--- MercadoPagoSDK.Test/Resources/CardTest.cs
+++ MercadoPagoSDK.Test/Resources/CardTest.cs
@@ -6,7 +6,7 @@ using System.Net;
 
 namespace MercadoPagoSDK.Test.Resources
 {
-    [TestFixture(Ignore = "Skipping")]
+    [TestFixture]
     public class CardTest
     {
         string AccessToken;
