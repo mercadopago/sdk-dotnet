a78730d4bcd0a0c7f460a9667568f9cb8c3b692b
Federico Berasategui <fberasategui@lexim.com.ar>
2018-07-25 02:46:08 -0300

    PayloadType enum is now internal

--- px-dotnet/Core/Annotations/PayloadType.cs
+++ px-dotnet/Core/Annotations/PayloadType.cs
@@ -8,7 +8,7 @@ namespace MercadoPago
     /// <summary>
     /// Enum that indicates the type of payload we will use in the future request.
     /// </summary>
-    public enum PayloadType
+    internal enum PayloadType
     {        
         NONE,
         JSON,
