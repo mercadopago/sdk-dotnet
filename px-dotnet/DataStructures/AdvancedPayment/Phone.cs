2a8fd0dec947daa657c6207828676d8e6873318f
Danilo Elias <danilo.elias@mercadolivre.com>
2020-04-22 17:18:01 -0300

    Adding advanced payments classes and methods

---
+++ px-dotnet/DataStructures/AdvancedPayment/Phone.cs
@@ -0,0 +1,23 @@
+﻿namespace MercadoPago.DataStructures.AdvancedPayment
+{
+    /// <summary>
+    /// Payer phone
+    /// </summary>
+    public class Phone
+    {
+        /// <summary>
+        /// Area code
+        /// </summary>
+        public string AreaCode { get; set; }
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Phone extension
+        /// </summary>
+        public string Extension { get; set; }
+    }
+}
