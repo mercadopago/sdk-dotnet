d0ba080427979f53ff648746750be5b8dca5e5d9
Danilo Elias <danilo.elias@mercadolivre.com>
2021-03-12 11:30:21 -0300

    Fixed payment.additional_info.items[].unit_price deserialization
    The error eccurred when the unit_price value was in exponential format (e.g., 5.0E8).

---
+++ px-dotnet/Common/StringDecimalConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace MercadoPago.Common
+{
+    public class StringDecimalConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            string value = (string)reader.Value;
+            if (string.IsNullOrEmpty(value))
+            {
+                return null;
+            }
+            return Convert.ToDecimal(float.Parse(value, CultureInfo.InvariantCulture));
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            decimal? decimalValue = (decimal?)value;
+            if (decimalValue.HasValue)
+            {
+                writer.WriteValue(decimalValue.Value.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
