de09bbc205de9b298f99380166d2424462fbc39e
rhames07 <rhames.lima@mercadolivre.com>
2022-07-06 14:33:16 -0300

    Add linked_to parameter

--- px-dotnet/DataStructures/Payment/PointOfInteraction.cs
+++ px-dotnet/DataStructures/Payment/PointOfInteraction.cs
@@ -16,6 +16,11 @@
         public string SubType { get; set; }
 
         /// <summary>
+        /// Linked To
+        /// </summary>
+        public string linkedTo { get; set; }
+
+        /// <summary>
         /// Appication Data
         /// </summary>
         public ApplicationData ApplicationData { get; set; }
