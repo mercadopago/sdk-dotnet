1797ae42615a6886887c40ab681a7acaf909a03a
aqudsilva_meli <raquel.rdsilva@mercadolivre.com>
2025-10-24 16:22:51 -0300

    fix: deprecating warning to address old fields

--- src/MercadoPago/Client/Preference/PreferenceReceiverAddressRequest.cs
+++ src/MercadoPago/Client/Preference/PreferenceReceiverAddressRequest.cs
@@ -1,5 +1,6 @@
 ﻿namespace MercadoPago.Client.Preference
 {
+    using System;
     using MercadoPago.Client.Common;
 
     /// <summary>
@@ -10,17 +11,35 @@
         /// <summary>
         /// Country.
         /// </summary>
-        public string Country { get; set; }
+        public string CountryName { get; set; }
 
         /// <summary>
         /// State.
         /// </summary>
-        public string State { get; set; }
+        public string StateName { get; set; }
 
         /// <summary>
         /// City.
         /// </summary>
-        public string City { get; set; }
+        public string CityName { get; set; }
+
+        /// <summary>
+        /// Country (deprecated, use CountryName instead).
+        /// </summary>
+        [Obsolete("Use CountryName instead. This property will be removed in a future version.")]
+        public string Country { get => CountryName; set => CountryName = value; }
+
+        /// <summary>
+        /// State (deprecated, use StateName instead).
+        /// </summary>
+        [Obsolete("Use StateName instead. This property will be removed in a future version.")]
+        public string State { get => StateName; set => StateName = value; }
+
+        /// <summary>
+        /// City (deprecated, use CityName instead).
+        /// </summary>
+        [Obsolete("Use CityName instead. This property will be removed in a future version.")]
+        public string City { get => CityName; set => CityName = value; }
 
         /// <summary>
         /// Floor.
